156bba895eed68c1ba14cb78eb14f85059159eed
Miguel477713 <131222467+Miguel477713@users.noreply.github.com>
2024-11-21 11:40:16 -0600

    Update SpeedMonitor.cpp

--- SpeedMonitor.cpp
+++ SpeedMonitor.cpp
@@ -8,6 +8,7 @@ using namespace std;
           void SpeedMonitor::monitor(){
             if(_speedThreshold  < 1 || _speedThreshold > 100){
               cout<<"_speedThreshold value must be in the ramge {1-100} "<<_speedThreshold<<endl;
+              this->_logger.Write("_speedThreshold value must be in the ramge {1-100} " + _speedThreshold);
             }
               
             BNFSpeedSensor speedSensorInstance;
@@ -15,6 +16,7 @@ using namespace std;
             int currentSpeedInKmph=speedSensorInstance.getCurrentSpeed();
             
             cout<<"Current Speed In Kmph "<<currentSpeedInKmph<<endl;
+                     this->_logger.Write(
             if(currentSpeedInKmph > _speedThreshold){
                   double mph = currentSpeedInKmph * 0.621371;
                   string message="Current Speed in Miles "+ to_string(mph);
