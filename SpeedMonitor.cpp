a6c4bdad25b6d30aebd25e877d0dec6ddab3fd28
Miguel477713 <131222467+Miguel477713@users.noreply.github.com>
2024-11-21 11:43:29 -0600

    Update SpeedMonitor.cpp

--- SpeedMonitor.cpp
+++ SpeedMonitor.cpp
@@ -7,7 +7,6 @@ using namespace std;
 
           void SpeedMonitor::monitor(){
             if(_speedThreshold  < 1 || _speedThreshold > 100){
-              cout<<"_speedThreshold value must be in the ramge {1-100} "<<_speedThreshold<<endl;
               this->_logger.Write("_speedThreshold value must be in the ramge {1-100} " + _speedThreshold);
             }
               
@@ -15,8 +14,7 @@ using namespace std;
             IOTCloudCommunicator cloudCommuniccator;
             int currentSpeedInKmph=speedSensorInstance.getCurrentSpeed();
             
-            cout<<"Current Speed In Kmph "<<currentSpeedInKmph<<endl;
-                     this->_logger.Write(
+            this->_logger.Write("Current Speed In Kmph " + currentSpeedInKmph);
             if(currentSpeedInKmph > _speedThreshold){
                   double mph = currentSpeedInKmph * 0.621371;
                   string message="Current Speed in Miles "+ to_string(mph);
