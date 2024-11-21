136ecfd056d9dc7da95785e2b678bf09a546d7c8
Miguel477713 <131222467+Miguel477713@users.noreply.github.com>
2024-11-21 11:38:39 -0600

    Update SpeedMonitor.h

--- SpeedMonitor.h
+++ SpeedMonitor.h
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <random> 
 #include <string>
+#include "ILogger.h"
+
 using namespace std;
 
 class SpeedMonitor{
-    private : int _speedThreshold;
-  
+    private : 
+        int _speedThreshold;
+        //Dependency
+        ILogger * _logger;
       public:
-          SpeedMonitor(int speedThreshold): _speedThreshold{speedThreshold} {}
+          SpeedMonitor(int speedThreshold, ILogger *logger): _speedThreshold{speedThreshold}, _logger{logger} {}
           void monitor();
 };
